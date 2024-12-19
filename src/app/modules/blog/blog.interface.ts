f181b699059ca95c29744d66321188b746c92b21
Sanchita Devi <thesanchitadevi@gmail.com>
2024-12-19 23:05:13 +0600

    blog initialize and modelschema

---
+++ src/app/modules/blog/blog.interface.ts
@@ -0,0 +1,7 @@
+import { Types } from 'mongoose';
+
+export interface IBlog {
+  title: string;
+  content: string;
+  author: Types.ObjectId; // User ID of the blog's author
+}
---
+++ src/app/modules/blog/blog.model.ts
@@ -0,0 +1,26 @@
+import { model, Schema } from 'mongoose';
+import { IBlog } from './blog.interface';
+
+const BlogSchema = new Schema<IBlog>(
+  {
+    title: {
+      type: String,
+      required: [true, 'Title is required'],
+      trim: true,
+    },
+    content: {
+      type: String,
+      required: [true, 'Content is required'],
+    },
+    author: {
+      type: Schema.Types.ObjectId,
+      ref: 'User', // Reference to the User model
+      required: [true, 'Author is required'],
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const BlogModel = model<IBlog>('Blog', BlogSchema);
